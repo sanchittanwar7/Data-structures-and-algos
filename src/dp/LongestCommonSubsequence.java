5419638e88652151c712aef5f57aa7d22eb0420a
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-02-07 20:33:26 +0530

    dp

--- src/dp/LongestCommonSubsequence.java
+++ src/dp/LongestCommonSubsequence.java
@@ -1,13 +1,50 @@
 package dp;
 
+import java.util.Arrays;
+
 public class LongestCommonSubsequence {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str1 = "ABCDGH";
+		String str2 = "AEDFHR";
+		System.out.println(lcs(str1, str2));
+	}
 
+	private static String lcs(String str1, String str2) {
+		int m = str1.length(), n = str2.length(), i, j = 0;
+		String str = "";
+		int arr[][] = new int[m + 1][n + 1];
+		for(i = 0; i <=m; i++){
+			for(j = 0; j <=n; j++){
+				if(i == 0 || j == 0)
+					arr[i][j] = 0;
+				else{
+					if(str1.charAt(i - 1) == str2.charAt(j - 1))
+						arr[i][j] = 1 + arr[i - 1][j - 1];
+					else
+						arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+				}
+			}
+		}
+		i = m;
+		j = n;
+		while(i > 0 && j > 0){
+			if(str1.charAt(i-1) == str2.charAt(j-1)){
+				str = str1.charAt(i-1) + str;
+				i--;
+				j--;
+			}
+			else{
+				if(arr[i-1][j] > arr[i][j-1])
+					i--;
+				else
+					j--;
+			}
+		}
+		return str;
 	}
 
 }
--- src/dp/LongestCommonSubstring.java
+++ src/dp/LongestCommonSubstring.java
@@ -6,8 +6,32 @@ public class LongestCommonSubstring {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str1 = "abab";
+		String str2 = "baba";
+		System.out.println(lcs(str1, str2));
+	}
 
+	private static String lcs(String str1, String str2) {
+		int m = str1.length(), n = str2.length(), i, j, ans = -1, max = 0;
+		int arr[][] = new int[m+1][n+1];
+		for(i = 0; i <= m; i++){
+			for(j = 0; j <= n; j++){
+				if(i == 0 || j == 0)
+					arr[i][j] = 0;
+				else{
+					if(str1.charAt(i-1) == str2.charAt(j-1)){
+						arr[i][j] = 1 + arr[i-1][j-1];
+						if(arr[i][j] >= max){
+							max = arr[i][j];
+							ans = j-1;
+						}
+					}
+					else
+						arr[i][j] = 0;
+				}
+			}
+		}
+		return str1.substring(ans - max + 1, ans + 1);
 	}
 
 }
