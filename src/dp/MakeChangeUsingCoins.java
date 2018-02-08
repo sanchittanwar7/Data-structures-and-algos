2bb3c9b4cc95e6170e0f3fa734caa31709a13328
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-02-08 23:52:43 +0530

    dp

--- src/dp/MakeChangeUsingCoins.java
+++ src/dp/MakeChangeUsingCoins.java
@@ -1,13 +1,23 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MakeChangeUsingCoins {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static long countWays(int S[], int m, int n){
+		long[] table = new long[n+1];
+		Arrays.fill(table, 0);   
+		table[0] = 1;
+		for (int i=0; i<m; i++)
+			for (int j=S[i]; j<=n; j++)
+				table[j] += table[j-S[i]];
+		return table[n];
+	}
+	public static void main(String args[]){
+		int arr[] = {1, 2, 3};
+		int m = arr.length;
+		int n = 4;
+		System.out.println(countWays(arr, m, n));
 	}
 
 }
--- src/dp/MinimumNumberOfCoins.java
+++ src/dp/MinimumNumberOfCoins.java
@@ -1,13 +1,37 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MinimumNumberOfCoins {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int denomitations[] = {2, 3, 5};
+		System.out.println(minCoins(denomitations, 10));
+	}
 
+	private static int minCoins(int[] denomitations, int n) {
+		int ans[] = new int[n+1], i, index, val;
+		for(i = 0; i <= n; i++)
+			ans[i] = Integer.MAX_VALUE;
+		for(int coins : denomitations)
+			ans[coins] = 1;
+		for(i = 0; i <= n; i++){
+			if(ans[i] != 1){
+				int min = Integer.MAX_VALUE - 1;
+				for(int j : denomitations){
+					index = i - j;
+					val = index < 0 ? Integer.MAX_VALUE : ans[index];
+					if(val < min)
+						min = val;
+				}
+				ans[i] = 1 + min;
+			}
+		}
+		System.out.println(Arrays.toString(ans));
+		return ans[n];
 	}
 
 }
